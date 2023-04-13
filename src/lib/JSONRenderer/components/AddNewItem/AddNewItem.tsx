e4bbec6183d9dfdde1137fc5b5491a41c84fc855
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2023-04-13 13:33:07 +0200

    Add field name input

--- src/lib/JSONRenderer/components/AddNewItem/AddNewItem.tsx
+++ src/lib/JSONRenderer/components/AddNewItem/AddNewItem.tsx
@@ -1,5 +1,6 @@
-import { FC, useCallback, useState } from 'react';
+import { ChangeEvent, FC, useCallback, useState } from 'react';
 import { classnames } from '../../utils/classnames';
+import { escapeFieldName } from '../../utils/fieldName';
 import { useJSONRendererContextActions } from '../../context';
 import { IProps } from './types';
 import { Button } from '../Button';
@@ -8,23 +9,57 @@ import { TypeSelector } from '../TypeSelector';
 import { TDataType } from '~/lib/types';
 
 const AddNewItem: FC<IProps> = ({ treeDescriptor }): JSX.Element => {
+  const [error, setError] = useState('');
+  const [fieldName, setFieldName] = useState('');
   const [selectedType, setSelectedType] = useState<TDataType>('string');
   const title = `Add New ${treeDescriptor.type === 'array' ? 'Item' : 'Field'}`;
 
   const { addNode } = useJSONRendererContextActions();
 
   const handleNewItem = useCallback(() => {
-    console.log('Add to:', treeDescriptor, selectedType);
-    addNode(treeDescriptor, selectedType, 'field');
-  }, [addNode, treeDescriptor, selectedType]);
+    const field = escapeFieldName(fieldName);
+    if (!field && treeDescriptor.type === 'object') {
+      setError("Field name can't be empty.");
+    } else {
+      console.log('Add to:', treeDescriptor, selectedType);
+      setError('');
+      addNode(treeDescriptor, selectedType, field);
+    }
+  }, [treeDescriptor, selectedType, addNode, fieldName]);
 
   const handleTypeChanged = useCallback((type: TDataType) => {
     setSelectedType(type);
+  }, []);
+  const fieldNameId = `${treeDescriptor.path}-field-name`;
+  const handleOnChange = useCallback(({ target }: ChangeEvent<HTMLInputElement>) => {
+    setFieldName(target.value);
   }, []);
 
   return (
     <div className={classnames('Element', 'Leaf', 'AddNewItem', `level-${treeDescriptor.level}`)}>
       <span className='Label'>{title}</span>
+      {treeDescriptor.type === 'object' && (
+        <span className={classnames('Value', 'Value-editable', !!error && 'invalid')}>
+          <label htmlFor={fieldNameId} hidden>
+            Add field name:
+          </label>
+          <input
+            id={fieldNameId}
+            placeholder='Add field name'
+            type={'text'}
+            value={fieldName}
+            onChange={handleOnChange}
+            aria-invalid={!!error}
+            aria-describedby={`${fieldNameId}-error-message`}
+          />
+          {!!error && (
+            <span className='error-message' id={`${fieldNameId}-error-message`}>
+              {error}
+            </span>
+          )}
+        </span>
+      )}
+      {treeDescriptor.type === 'array' && <span className='Value'>0</span>}
       <Toolbox>
         <TypeSelector id={treeDescriptor.path} type={selectedType} onChange={handleTypeChanged} />
         <Button
