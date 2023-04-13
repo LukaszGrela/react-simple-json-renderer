31ffa2cdc232ac80427295211d1e8d3c031a07cb
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2023-04-13 15:12:39 +0200

    Use children function

--- src/lib/JSONRenderer/components/Label/Label.tsx
+++ src/lib/JSONRenderer/components/Label/Label.tsx
@@ -3,12 +3,12 @@ import { classnames } from '../../utils/classnames';
 import { unescapeFieldName } from '../../utils/fieldName';
 import { IProps } from './types';
 
-const Label: FC<IProps> = ({ treeDescriptor, className }): JSX.Element => {
+const Label: FC<IProps> = ({ treeDescriptor, className, children }): JSX.Element => {
   const escapedLabel = useMemo(() => unescapeFieldName(treeDescriptor.key), [treeDescriptor.key]);
 
   return (
     <span className={classnames('Label', className)} title={escapedLabel}>
-      {escapedLabel}
+      {children ? children(escapedLabel) : escapedLabel}
     </span>
   );
 };
