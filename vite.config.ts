2185fbd2a15bb308c4087f3c6c8952b248b107c2
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2023-04-16 00:07:46 +0200

    Update build script for module export

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,50 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
+import path from 'path';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require('./package.json');
+
+const getPackageName = () => {
+  return packageJson.name;
+};
+
+const getPackageNameCamelCase = () => {
+  try {
+    return getPackageName().replace(/-./g, (char: string): string => char[1].toUpperCase());
+  } catch (err) {
+    throw new Error('Name property in package.json is missing.');
+  }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  base: './',
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: getPackageNameCamelCase(),
+
+      formats: ['es', 'umd'],
+      fileName: (format) => `${getPackageName()}.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 });
