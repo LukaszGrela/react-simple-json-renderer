9bbe7dffeebf4214f07caa56c511dbcfa15cb5f3
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2023-09-01 12:34:20 +0200

    VisuallyHidden component

---
+++ src/lib/JSONRenderer/components/VisuallyHidden/VisuallyHidden.tsx
@@ -0,0 +1,22 @@
+import { FC, PropsWithChildren } from 'react';
+
+const VisuallyHidden: FC<PropsWithChildren<any>> = ({ children }): JSX.Element => {
+  return (
+    <span
+      style={{
+        border: 0,
+        clip: 'rect(0,0,0,0)',
+        height: '1px',
+        margin: '-1px',
+        overflow: 'hidden',
+        padding: 0,
+        position: 'absolute',
+        width: '1px',
+      }}
+    >
+      {children}
+    </span>
+  );
+};
+
+export default VisuallyHidden;
